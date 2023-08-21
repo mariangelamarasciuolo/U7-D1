8bed64839323d9396794bdce3de0dd6548f4153f
mariangelamarasciuolo <130611642+mariangelamarasciuolo@users.noreply.github.com>
2023-08-21 21:54:54 +0200

    complete

--- js.js
+++ js.js
@@ -18,32 +18,42 @@ const comparazioneEta = giacomoporetti.differenzaEta(aldoBaglio);
 console.log(comparazioneEta);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-class Pets{
-    constructor(petName, ownerName, species, breed) {
-        this.petName = petName
-        this.ownerName = ownerName
-        this.species = species
-        this.breed = breed
-    }
-    stessoPadrone(altroPet){
-        return this.ownerName === altroPet.ownerName
-    }
-    
+const pet = [];
+let count = 0;
+class Pet {
+  constructor(petName, ownerName, species, breed) {
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.species = species;
+    this.breed = breed;
+  }
 }
-const petForm = document.getElementById("petForm")
-const petsList = document.getElementById("petsList")
 
-petForm.addEventListener("submit", function(event){
-event.preventDefault();
-let petName = document.getElementById("petName").value;
-let ownerName = document.getElementById("ownerName").value;
-let species = document.getElementById("species").value;
-let breed = document.getElementById("breed").value;
+function stessoPadrone(altroPet) {
+  if (this.ownerName.value === altroPet.ownerName) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-let newPet = new Pets(petName, ownerName, species, breed);
-const listItem = document.createElement("li")
-listItem.textContent = `Nome: ${newPet.petName}, Proprietario: ${newPet.ownerName}, specie: ${newPet.species}, Razza ${newPet.breed} `
-petsList.appendChild(listItem);
+const petForm = document.getElementById("petForm");
+const petsList = document.getElementById("petsList");
 
+petForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let petName = document.getElementById("petName").value;
+  let ownerName = document.getElementById("ownerName").value;
+  let species = document.getElementById("species").value;
+  let breed = document.getElementById("breed").value;
+
+  let newPet = new Pet(petName, ownerName, species, breed);
+  pet.push(newPet);
+  if (pet.length > 1) {
+    console.log(stessoPadrone(pet[count]));
+    count++;
+  }
+  const listItem = document.createElement("li");
+  listItem.textContent = `Nome: ${newPet.petName}, Proprietario: ${newPet.ownerName}, specie: ${newPet.species}, Razza ${newPet.breed} `;
+  petsList.appendChild(listItem);
 });
